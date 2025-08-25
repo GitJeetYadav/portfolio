fc171b68412292a98791adb64ba5dd5ebaaa7008
Jeet yadav <jitendra.4822@gmail.com>
2025-08-25 16:44:15 +0530

    remove firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 // import { environment } from '../environments/environment';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+// import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+// import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
@@ -11,8 +11,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
-    provideFirestore(() => getFirestore()),
-    provideClientHydration(withEventReplay()),
+    // provideFirestore(() => getFirestore()),
+    // provideClientHydration(withEventReplay()),
     // provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
   ],
 };
